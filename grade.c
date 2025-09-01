dd2b71e60b1e0c2fa072f606fb30a5cf6df7fee3
eekarshvats-bit <eekarshvats@gmail.com>
2025-09-01 23:24:26 +0530

    Add files via upload

---
+++ grade.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main ()
+{
+ float s1, s2, s3, s4, s5, per;
+
+printf("Enter marks of 5 subjects\n");
+scanf("%f %f %f %f %f",&s1, &s2, &s3, &s4, &s5);
+
+per= ( s1 + s2+ s3+ s4+ s5) /5.0;
+
+if (per>=60)
+{ printf ("Grade A\n");
+}
+else
+if (per>=50)
+{ printf ("Grade B\n");
+}
+else
+if (per>=40)
+{ printf ("Grade C\n");
+}
+else
+{ printf ("Failed!\n");
+}
+return 0;
+}

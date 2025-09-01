74502d97c7aa55caa2b0689810037f4f072b6dd3
eekarshvats-bit <eekarshvats@gmail.com>
2025-09-01 23:47:47 +0530

    Add files via upload

---
+++ trafficlight.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 0; i < 3; i++) {
+        printf("\nRED - Stop\n");
+        printf("Wait...\n");
+
+        printf("\nYELLOW - Get Ready\n");
+        printf("Wait...\n");
+
+        printf("\nGREEN - Go\n");
+        printf("Wait...\n");
+    }
+
+    return 0;
+}
